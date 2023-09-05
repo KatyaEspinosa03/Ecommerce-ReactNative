1baad27230a8e50f84af499a2d7f2414118fd083
KatyaEspinosa03 <katyamiranda03@hotmail.com>
2023-09-05 10:42:54 -0600

    agregue category item en el el componente de categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,17 +1,16 @@
 import { StyleSheet, Text, View, FlatList} from 'react-native'
 import React from 'react'
 import dataCategories from '../data/dataCategories'
+import CategoryItem from './CategoryItem'
 
 const Categories = () => {
   return (
     <View style={styles.container}>
+
    <FlatList 
    data={dataCategories}
    keyExtractor={category => category}
-   renderItem={({item}) => (
-   <View style={{backgroundColor:"red"}}> 
-    <Text> {item} </Text>
-   </View>)}
+   renderItem={({item}) => <CategoryItem category={item} />}
    />
 
     </View>
