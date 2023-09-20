822c491d47a90eb8d07ea1130e36b956fa3a4403
KatyaEspinosa03 <katyamiranda03@hotmail.com>
2023-09-20 10:44:48 -0600

    importe la libreria feather de vector icons y agregue iconos al bottomtab

--- src/navegation/BottomTabNavigator.jsx
+++ src/navegation/BottomTabNavigator.jsx
@@ -3,7 +3,7 @@ import CartNavigator from './CartNavigator'
 import StackNavigator from "./StackNavigator";
 import { StyleSheet } from "react-native";
 import { colors } from "../constants/colors";
-
+import Feather from '@expo/vector-icons/Feather'
 
 const BottomTab = createBottomTabNavigator()
 
@@ -18,7 +18,16 @@ function BottomTabNavigator() {
     >
 
         <BottomTab.Screen name="HomeNav" component={StackNavigator} /> 
-        <BottomTab.Screen name="CartNav" component={CartNavigator}/>
+        <BottomTab.Screen name="CartNav" component={CartNavigator}
+                options={{
+                    tabBarIcon: ({focused}) => (
+                        <Feather 
+                        name='shopping-bag' 
+                        size={24} 
+                        color={focused ? colors.secondary : colors.primary }
+                        />
+                    ),
+                }}/>
 
 
     </BottomTab.Navigator>
