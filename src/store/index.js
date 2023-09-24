b2b6b5a1b5a1cfa3cecaa610720a53c5f3f95c55
KatyaEspinosa03 <katyamiranda03@hotmail.com>
2023-09-24 14:52:23 -0600

    agruegue el boton de agregar al componente de details

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,6 @@ export const store = configureStore({
   reducer: {
     // para usar todo lo implementado en counterSlice se utilizara el nombre de counter
     counter: counterSlice,
-    shop: shopSlice
+    shop: shopSlice,
   },
 })
